b41688017548a6826e3bf406427f8f165ec58a49
riumr <atrium_grate.0j@icloud.com>
2024-03-08 16:41:01 +0900

    study : static, instance, class 멤버 선언 및 사용

---
+++ src/day4/exercise/Circle.java
@@ -0,0 +1,29 @@
+package day4.exercise;
+
+public class Circle {
+  // 정적 멤버, static 멤버, class 멤버
+  static int numberOfCircles;
+
+  // 인스턴스 멤버
+  // 필드
+  int radius;
+  String color="";
+
+  // 메서드
+  // 정적 메서드
+  static void staticMethod1(){
+    System.out.println(numberOfCircles);
+  }
+
+  // 인스턴스 메서드
+  double instanceMethod1(){
+    return 1234;
+  }
+
+  // 생성자
+  Circle(int radius,String color){
+    this.radius=radius;
+    this.color=color;
+    System.out.println(color+"의 크기가 "+radius+"인 circle이 "+ ++numberOfCircles+"개 생성되었습니다.");
+  }
+}
---
+++ src/day4/exercise/CircleDemo.java
@@ -0,0 +1,8 @@
+package day4.exercise;
+
+public class CircleDemo {
+  public static void main(String[] args) {
+    Circle c1 = new Circle(100,"빨간색");
+    Circle c2 = new Circle(50,"노란색");
+  }
+}
