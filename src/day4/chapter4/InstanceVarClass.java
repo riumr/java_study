79a0902b6c9952c09980525991d38de337dd3d6c
riumr <atrium_grate.0j@icloud.com>
2024-03-08 10:23:45 +0900

    study : instance 변수 생성 및 class 변수와 비교

---
+++ src/day4/chapter4/InstanceVarClass.java
@@ -0,0 +1,13 @@
+package day4.chapter4;
+
+public class InstanceVarClass {
+  static int common;
+  int instanceVar;
+  
+  static void methodStatic(){
+    System.out.println("staic method 입니다.");
+  }
+  void method2(){
+    System.out.println("method2 입니다.");
+  }
+}
