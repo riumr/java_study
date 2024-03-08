34ef73d6861f68843c2aa3a336d2bb55edb71ae7
riumr <atrium_grate.0j@icloud.com>
2024-03-08 10:24:07 +0900

    study : 지역변수 설명

---
+++ src/day4/chapter4/LocalVarDemo.java
@@ -0,0 +1,13 @@
+package day4.chapter4;
+
+public class LocalVarDemo {
+  public static void main(String[] args) {
+    int a = 0;
+    System.out.println(a);
+    int c = 10;
+    System.out.println(a + c);
+    for (int e = 0; e < 10; e++) {
+      System.out.println(e);
+    }
+  }
+}
