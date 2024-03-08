eb224857cea58d5424c08d0ee4fa8293419f6a3f
riumr <atrium_grate.0j@icloud.com>
2024-03-08 15:48:40 +0900

    study : this를 통한 자기참조 실습

---
+++ src/day4/chapter4/PersonDemo.java
@@ -0,0 +1,9 @@
+package day4.chapter4;
+
+public class PersonDemo {
+  public static void main(String[] args) {
+    Person person = new Person();
+    person.setName("민국").setAge(21).sayHello();
+
+  }
+}
