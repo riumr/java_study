6c5f7d55d1a59f5cb3b2fff697c49220d0c6a13b
riumr <108637691+riumr@users.noreply.github.com>
2024-03-11 12:00:03 +0900

    study : 배열 생성 방법 실습
    - 배열도 type을 지정해야 한다

---
+++ src/day5/chapter5/ArrayDemo.java
@@ -0,0 +1,15 @@
+package day5.chapter5;
+
+public class ArrayDemo {
+  public static void main(String[] args) {
+//    int scores[] = new int[5];
+//    for (int i = 0; i < 5; i++) {
+//      scores[i]=100;
+//      System.out.println(scores[i]);
+//    }
+
+    int[] scores = {100, 90, 80, 70, 60};
+    int[] scores1;
+    scores1 = new int[]{100,90,80,70,60};
+  }
+}
