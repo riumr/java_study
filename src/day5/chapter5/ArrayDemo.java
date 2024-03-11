80f39e5186887815e9ea8a07e7780c7154de0c6a
riumr <108637691+riumr@users.noreply.github.com>
2024-03-11 12:35:30 +0900

    study : Array 선언법

--- src/day5/chapter5/ArrayDemo.java
+++ src/day5/chapter5/ArrayDemo.java
@@ -11,5 +11,12 @@ public class ArrayDemo {
     int[] scores = {100, 90, 80, 70, 60};
     int[] scores1;
     scores1 = new int[]{100,90,80,70,60};
+
+    System.out.println("세번째 학생의 점수는 "+scores[2]+"입니다");
+
+    scores[2] = 85;
+
+    System.out.println("세번째 학생의 점수는 "+scores[2]+"입니다");
+    System.out.println(scores.length);
   }
 }
