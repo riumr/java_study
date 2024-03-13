e474d457d70402623beb4ec3847c19c42f786c8d
riumr <108637691+riumr@users.noreply.github.com>
2024-03-13 17:14:07 +0900

    study : super를 사용한 부모 객체 메소드 상속

--- src/day7/chapter6/Ball.java
+++ src/day7/chapter6/Ball.java
@@ -14,6 +14,8 @@ public class Ball extends Circle{
   @Override // annotation이 없어도 작동하며, annotation은 override하고 있다는 점을 컴파일러에 알리는 역할을 한다.
   void findArea() {
     System.out.println("넓이는 4*3.14*반지름의 제곱입니다.");
+    //
+    super.findArea();
   }
 
   void findVolume(){
