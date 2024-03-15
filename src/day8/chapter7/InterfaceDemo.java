ce449341c297a403cbc71372680b01ffc589a476
riumr <108637691+riumr@users.noreply.github.com>
2024-03-15 10:09:09 +0900

    study : 인터페이스 주석 수정
    - 상속 -> 구현

--- src/day8/chapter7/InterfaceDemo.java
+++ src/day8/chapter7/InterfaceDemo.java
@@ -1,10 +1,18 @@
 package day8.chapter7;
 
-// Interface 상속받은 객체 확인
+// Interface 구현한 객체 확인
 public class InterfaceDemo {
   public static void main(String[] args) {
     Hello hello = new Hello();
     hello.sayHello();
     System.out.println(hello.max);
+
+    GoodBye goodBye = new GoodBye();
+    goodBye.sayHello();
+    goodBye.say();
+
+    SaySomething saySomething = new SaySomething();
+    saySomething.say();
+    saySomething.sayHello();
   }
 }
