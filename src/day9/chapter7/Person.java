e6faff9344b32d2f9d5bc1b8035e1ba935212359
riumr <108637691+riumr@users.noreply.github.com>
2024-03-15 16:41:26 +0900

    refact : colon 위치 변경

--- src/day9/chapter7/Person.java
+++ src/day9/chapter7/Person.java
@@ -15,8 +15,7 @@ public class Person implements Comparable {
 
   @Override
   public String toString() {
-    return "Person [이름=" + name + ",나이=" + age + ",키=" + height + ",몸무게=" + weight + "]"
-        ;
+    return "Person [이름=" + name + ",나이=" + age + ",키=" + height + ",몸무게=" + weight + "]";
   }
 
   @Override
