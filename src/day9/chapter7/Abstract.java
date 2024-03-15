2fcc75abe250e9a3ed4b2a02cc6b9bd4c325bf99
riumr <108637691+riumr@users.noreply.github.com>
2024-03-15 14:37:51 +0900

    study : 추상클래스로 생성된 인스턴스 확인

---
+++ src/day9/chapter7/Abstract.java
@@ -0,0 +1,10 @@
+package day9.chapter7;
+
+// 추상 클래스
+abstract class Abstract {
+  int i;
+  abstract void show();
+  public Abstract(int i){
+    this.i=i;
+  }
+}
---
+++ src/day9/chapter7/AbstractTest.java
@@ -0,0 +1,9 @@
+package day9.chapter7;
+
+// 추상클래스로 생성된 인스턴스 확인
+public class AbstractTest {
+  public static void main(String[] args) {
+    Concreate c = new Concreate(100,50);
+    c.show();
+  }
+}
