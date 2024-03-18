48e8176aa82605024a8b67525b146d35b8d17b4e
riumr <108637691+riumr@users.noreply.github.com>
2024-03-18 15:59:42 +0900

    study : Wrapper 클래스

---
+++ src/day10/chapter8/WrapperDemo.java
@@ -0,0 +1,14 @@
+package day10.chapter8;
+
+// Wrapper 클래스
+public class WrapperDemo {
+  public static void main(String[] args) {
+    Integer bi1 = 10;
+    Integer.parseInt("10");
+    int i = bi1+10;
+    System.out.println(i);
+    Double di = 3.14;
+    double d = di + 3.14;
+    System.out.println(d);
+  }
+}
