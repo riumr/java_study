789381937c7f9ee5670ee087ce048909e66f4438
riumr <108637691+riumr@users.noreply.github.com>
2024-03-18 11:12:27 +0900

    study : Class 메소드들

---
+++ src/day10/chapter8/ClassDemo.java
@@ -0,0 +1,16 @@
+package day10.chapter8;
+
+// Class 메소드들
+public class ClassDemo {
+  public static void main(String[] args) {
+    Keyboard k = new Keyboard("로지텍 키보드");
+
+    System.out.println(k.getClass());
+
+    Class c = k.getClass();
+    System.out.println(c.getName());
+    System.out.println(c.getSimpleName());
+    System.out.println(c.getTypeName());
+    System.out.println(c.getPackage().getName());
+  }
+}
