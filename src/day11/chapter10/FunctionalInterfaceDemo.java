be8236bf40b4b2d30489474fd4f87117d08c25bd
riumr <108637691+riumr@users.noreply.github.com>
2024-03-19 16:48:32 +0900

    study : lambda식으로 함수형 interface 메소드 사용

--- src/day11/chapter10/FunctionalInterfaceDemo.java
+++ src/day11/chapter10/FunctionalInterfaceDemo.java
@@ -2,7 +2,7 @@ package day11.chapter10;
 
 import java.util.Comparator;
 
-// 람다식을 사용한 메소드 overriding
+// lambda식으로 함수형 interface 메소드 사용
 public class FunctionalInterfaceDemo {
   public static void main(String[] args) {
     Comparator<String> c1 = (o1,o2) -> o1.length()-o2.length();
