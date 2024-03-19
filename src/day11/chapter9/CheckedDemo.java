de762ac50db231fe2b37ebb3a4a1112532aa0f9a
riumr <108637691+riumr@users.noreply.github.com>
2024-03-19 11:15:36 +0900

    study : 일반 예외. 예외 처리 코드를 반드시 추가해야 한다

---
+++ src/day11/chapter9/CheckedDemo.java
@@ -0,0 +1,8 @@
+package day11.chapter9;
+
+// 일반 예외. 예외 처리 코드를 반드시 추가해야 한다
+public class CheckedDemo {
+  public static void main(String[] args) throws InterruptedException {
+    Thread.sleep(1000);
+  }
+}
