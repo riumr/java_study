cba1403dcf90bdec4e29862b20ed41bb98534c0a
riumr <108637691+riumr@users.noreply.github.com>
2024-03-27 17:19:20 +0900

    study : 테이블별 DAO 생성
    - mysql-java crud 실습

---
+++ src/day17/LibraryProject/BookDAO.java
@@ -0,0 +1,127 @@
+package day17.LibraryProject;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BookDAO {
+
+  public List<Book> bookList() throws SQLException {
+    Connection con = LibraryDBConnection.getConnection();
+    String sql = "select id,library_id, book_name,writer_name, publisher from books;";
+    PreparedStatement ps = con.prepareStatement(sql);
+    ResultSet rs = ps.executeQuery();
+    List<Book> bookList = new LinkedList<>();
+    if (rs != null) {
+      while (rs.next()) {
+        Book book = new Book(rs.getInt(1), rs.getInt(2), rs.getString(3),
+            rs.getString(4), rs.getString(5), "", "", "", "");
+        bookList.add(book);
+      }
+    }
+    return bookList;
+  }
+
+  public List<Book> bookListByName(String bookname) throws SQLException {
+    Connection con = LibraryDBConnection.getConnection();
+    String sql = "select id,library_id, book_name,writer_name, publisher from books where book_name like ?;";
+    PreparedStatement ps = con.prepareStatement(sql);
+    ps.setString(1, "%"+bookname+"%");
+    ResultSet rs = ps.executeQuery();
+    List<Book> bookList = new LinkedList<>();
+    if (rs != null) {
+      while (rs.next()) {
+        Book book = new Book(rs.getInt(1), rs.getInt(2), rs.getString(3),
+            rs.getString(4), rs.getString(5), "", "", "", "");
+        bookList.add(book);
+      }
+    } else {
+      System.out.println("찾는 도서가 없습니다.");
+    }
+    return bookList;
+  }
+
+  public List<Book> bookListByWriter(String writer) throws SQLException {
+    Connection con = LibraryDBConnection.getConnection();
+    String sql = "select id,library_id, book_name,writer_name, publisher from books where writer_name like ?;";
+    PreparedStatement ps = con.prepareStatement(sql);
+    ps.setString(1, writer);
+    ResultSet rs = ps.executeQuery();
+    List<Book> bookList = new LinkedList<>();
+    if (rs != null) {
+      while (rs.next()) {
+        Book book = new Book(rs.getInt(1), rs.getInt(2), rs.getString(3),
+            rs.getString(4), rs.getString(5), "", "", "", "");
+        bookList.add(book);
+      }
+    } else {
+      System.out.println("찾는 도서가 없습니다.");
+    }
+    return bookList;
+  }
+
+  public List<Book> bookListByPublisher(String publisher) throws SQLException {
+    Connection con = LibraryDBConnection.getConnection();
+    String sql = "select id,library_id, book_name,writer_name, publisher from books where publisher like concat ('%',?,'%');";
+    PreparedStatement ps = con.prepareStatement(sql);
+    ps.setString(1, publisher);
+    ResultSet rs = ps.executeQuery();
+    List<Book> bookList = new LinkedList<>();
+    if (rs != null) {
+      while (rs.next()) {
+        Book book = new Book(rs.getInt(1), rs.getInt(2), rs.getString(3),
+            rs.getString(4), rs.getString(5), "", "", "", "");
+        bookList.add(book);
+      }
+    } else {
+      System.out.println("찾는 도서가 없습니다.");
+    }
+    return bookList;
+  }
+
+  public void bookCreate(Book book) throws SQLException {
+    Connection con = LibraryDBConnection.getConnection();
+    String sql = "insert into books (id,library_id,book_name,writer_name,publisher,classifier,isbn,kubun,status) values (?,?,?,?,?,?,?,?,?);";
+    PreparedStatement ps = con.prepareStatement(sql);
+    ps.setInt(1, book.getId());
+    ps.setInt(2, book.getLibraryId());
+    ps.setString(3, book.getBookName());
+    ps.setString(4, book.getWriterName());
+    ps.setString(5, book.getPublisher());
+    ps.setString(6, "");
+    ps.setString(7, book.getIsbn());
+    ps.setString(8, "");
+    ps.setString(9, "");
+    if (ps.executeUpdate() == 1) {
+      System.out.println("도서 정보 추가 성공");
+    } else {
+      System.out.println("도서 정보 추가 실패");
+    }
+    ps.close();
+    con.close();
+  }
+
+  public void bookUpdate(Book book, String status) {
+  }
+
+  public boolean bookCheck(Book book) {
+    return false;
+  }
+
+  public void bookDelete(Book book) throws SQLException {
+    Connection con = LibraryDBConnection.getConnection();
+    String sql = "Delete from books where id=?;";
+    PreparedStatement ps = con.prepareStatement(sql);
+    ps.setInt(1, book.getId());
+    if (ps.executeUpdate() == 1) {
+      System.out.println("도서 정보 삭제 성공");
+    } else {
+      System.out.println("도서 정보 삭제 실패");
+    }
+    ps.close();
+    con.close();
+  }
+}
