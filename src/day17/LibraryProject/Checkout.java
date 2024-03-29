2b4613614d050615af03a750a278d79f3ce5883a
riumr <108637691+riumr@users.noreply.github.com>
2024-03-29 16:29:44 +0900

    study : 대출 및 반납 관련 Checkout 클래스 생성

--- src/day17/LibraryProject/Checkout.java
+++ src/day17/LibraryProject/Checkout.java
@@ -3,7 +3,19 @@ package day17.LibraryProject;
 import java.util.Date;
 
 public class Checkout {
-  private int id;
+  public Checkout(Integer id, int libraryId, int bookId, int userId, Date checkoutDate, Date returnDate, boolean renewed, Date renewReturnDate, Date checkinDate) {
+    this.id = id;
+    this.libraryId = libraryId;
+    this.bookId = bookId;
+    this.userId = userId;
+    this.checkoutDate = checkoutDate;
+    this.returnDate = returnDate;
+    this.renewed = renewed;
+    this.renewReturnDate = renewReturnDate;
+    this.checkinDate = checkinDate;
+  }
+
+  private Integer id;
   private int libraryId;
   private int bookId;
   private int userId;
@@ -12,4 +24,45 @@ public class Checkout {
   private boolean renewed;
   private Date renewReturnDate;
   private Date checkinDate;
+
+  public int getId() {
+    return id;
+  }
+
+  public int getLibraryId() {
+    return libraryId;
+  }
+
+  public int getBookId() {
+    return bookId;
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public Date getCheckoutDate() {
+    return checkoutDate;
+  }
+
+  public Date getReturnDate() {
+    return returnDate;
+  }
+
+  public boolean isRenewed() {
+    return renewed;
+  }
+
+  public Date getRenewReturnDate() {
+    return renewReturnDate;
+  }
+
+  public Date getCheckinDate() {
+    return checkinDate;
+  }
+
+  @Override
+  public String toString() {
+    return "checkout ["+id+","+libraryId+","+bookId+","+userId+","+checkoutDate+","+returnDate+"]";
+  }
 }
